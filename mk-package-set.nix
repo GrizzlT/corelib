15a932bde270bf390c5b40b2c16e80a4a1907389
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-01-06 23:08:47 +0100

    feat: package-set local override

--- mk-package-set.nix
+++ mk-package-set.nix
@@ -6,24 +6,34 @@ let
       (builtins.attrNames set);
 
   fix = f: let x = f x; in x;
-
+  combineOverride = f: g: prev: let
+    prev' = f prev;
+  in g (prev // { self = prev.self // prev'; });
 
   # TODO: apply overrides
-  mkPackageSet = { packages, dependencies ? {}, overrides ? {} }: {
-    # Return the packages in this set resolved in their ddependency context
-    apply = pkgs: depMappings: let
-      toFix = self: let
-        pkgsSelf = pkgs // { inherit self; };
-        pkgsInput = builtins.foldl' (acc: mapping: acc // { ${mapping.name} = acc.${mapping.depName}; }) pkgsSelf depMappings;
-        resolved = builtins.mapAttrs (_: pkg: pkg { pkgs = pkgsInput; }) packages;
-      in resolved;
-    in fix toFix;
+  mkPackageSet = { packages, dependencies ? {} }: let
+    withFunctors = raw: let
+      createFunctors = override0: let
+        __original = _: raw // (createFunctors (_: {}));
+        withOverride = override: raw //
+          (createFunctors (combineOverride override0 override));
+        apply = pkgs: depMappings: let
+          toFix = self: let
+            pkgsArg = this: let
+              pkgsSelf = pkgs // { self = this; };
+              pkgsInput = builtins.foldl' (acc: mapping: acc // { ${mapping.name} = acc.${mapping.depName}; }) pkgsSelf depMappings;
+            in pkgsInput;
+
+            resolved = builtins.mapAttrs (_: pkg: pkg { pkgs = pkgsArg self; }) packages;
+          in resolved // override0 (pkgsArg resolved);
+        in fix toFix;
+      in { inherit withOverride apply __original; };
+    in raw // createFunctors (_: {});
+  in withFunctors {
+    inherit packages dependencies;
 
     # TODO: apply overrides
     allDeps = foldlAttrs (acc: depName: dep: acc ++ dep.allDeps ++ [{ name = depName; inherit dep; }]) [] dependencies;
-
-    allOverrides = foldlAttrs (acc: _: dep: acc ++ dep.allOverrides) [] dependencies
-      ++ (if builtins.length (builtins.attrValues overrides) == 0 then [] else [overrides]);
   };
 in
   mkPackageSet
