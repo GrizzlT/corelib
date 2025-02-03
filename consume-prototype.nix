46eaac1dbbb3c2444a008efaaff2a2be914a5527
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-02-03 23:48:59 +0100

    chore: remove old implementation

--- consume-prototype.nix
+++ consume-prototype.nix
@@ -85,39 +85,6 @@ let
     children = recurseIntoDeps acc setName value.dependencies;
     resolved = resolveDep depMappings.${setName} setName value;
   in children // { ${depMappings.root.${name}} = resolved; }) {} pkgSets;
-
-
-  consumePkgs = pkgSets: let
-    expanded = builtins.mapAttrs (_: v: { pkg = v; deps = v.allDeps; }) pkgSets;
-
-    applyDep = meta: name: dep: let
-      needsMapping = meta.pkgs ? ${name};
-      counter = if needsMapping then meta.counter + 1 else meta.counter;
-      depName = if needsMapping then name + "_dep_${toString meta.counter}" else name;
-    in {
-      inherit counter;
-      pkgs = meta.pkgs // {
-        ${depName} = dep.apply meta.pkgs meta.depMappings;
-      };
-      depMappings = meta.depMappings ++ (if needsMapping then [{
-        inherit name depName;
-      }] else []);
-    };
-
-    consumed = foldlAttrs (meta: name: set: meta // (let
-      depsResult = builtins.foldl' (meta2: dep: applyDep meta2 dep.name dep.dep) (meta // { depMappings = []; }) set.deps;
-
-      needsMapping = meta.pkgs ? ${name};
-      counter = if needsMapping then depsResult.counter + 1 else depsResult.counter;
-      pkgName = if needsMapping then name + "_pkg_${toString depsResult.counter}" else name;
-    in {
-      inherit counter;
-      pkgs = meta.pkgs // {
-        ${pkgName} = set.pkg.apply depsResult.pkgs depsResult.depMappings;
-      };
-    })) { counter = 0; pkgs = {}; } expanded;
-
-  in consumed;
 in {
-  inherit consumePkgs depMapping resolveLibs resolvePkgs;
+  inherit depMapping resolveLibs resolvePkgs;
 }
